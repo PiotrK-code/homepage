22f616194e965918cf3fbb98c26ac278fde7f1cd
Piotr Kowalski <piokowam@gmail.com>
2021-05-03 15:03:37 +0200

    adding functions to js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-console.log("Cześć!");
+{
+    const welcome = () => {
+        console.log("Cześć!");
+    }
 
-let changeBackgroundButton = document.querySelector(".js__changeBackground");
-let body = document.querySelector(".body");
-let nextColorName = document.querySelector(".js__nextColorName")
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const nextColorName = document.querySelector(".js__nextColorName")
+        body.classList.toggle("js__greyBackground");
+        nextColorName.innerText = body.classList.contains("js__greyBackground") ? "białe" : "szare";
+    }
 
-changeBackgroundButton.addEventListener("click", () => {
-body.classList.toggle("js__greyBackground");
-nextColorName.innerText = body.classList.contains("js__greyBackground") ? "białe" : "szare";
+    const init = () => {
+        const changeBackgroundButton = document.querySelector(".js__changeBackground");
+        changeBackgroundButton.addEventListener("click", toggleBackground);
 
-});
+        welcome();
+    }
+
+    init();
+   
+}
